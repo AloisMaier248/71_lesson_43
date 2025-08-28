0f0b543220e830e147a2db1af604ee59f04393b7
AloisMaier248 <maier.alois@outlook.com>
2025-08-28 20:58:34 +0200

    Add new Class - Test2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("This is the first version");
+        System.out.println("Update of class Test1");
     }
 }
